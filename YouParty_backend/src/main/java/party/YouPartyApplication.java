97d047d7ea1d8e7aab4390f587205f25e250266a
Marek Pokropiński <marek.pokropinski@outlook.com>
2018-09-14 23:58:23 +0200

    initial commit

---
+++ YouParty_backend/src/main/java/party/YouPartyApplication.java
@@ -0,0 +1,12 @@
+package party;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouPartyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouPartyApplication.class, args);
+	}
+}
---
+++ YouParty_backend/src/test/java/party/YouPartyApplicationTests.java
@@ -0,0 +1,16 @@
+package party;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class YouPartyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
