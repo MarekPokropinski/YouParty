e8df4ac95ecc03bc512787bf3311a46d1e0ae042
Marek Pokropiński <marek.pokropinski@outlook.com>
2018-09-22 10:10:23 +0200

    Bugfix

--- YouParty_backend/src/main/java/party/lobby/Lobby.java
+++ YouParty_backend/src/main/java/party/lobby/Lobby.java
@@ -1,7 +1,7 @@
 package party.lobby;
 
 import java.util.ArrayDeque;
-import java.util.Queue;
+import java.util.Collection;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
@@ -12,18 +12,18 @@ import party.video.Video;
 @Entity
 public class Lobby extends Basemodel {
 	@OneToMany
-	private Queue<Video> videoQueue;
+	private Collection<Video> videoQueue;
 
 	public Lobby() {
 		super();
 		videoQueue = new ArrayDeque<Video>();
 	}
 
-	public Queue<Video> getVideoQueue() {
+	public Collection<Video> getVideoQueue() {
 		return videoQueue;
 	}
 
-	public void setVideoQueue(Queue<Video> videoQueue) {
+	public void setVideoQueue(Collection<Video> videoQueue) {
 		this.videoQueue = videoQueue;
 	}
 }
