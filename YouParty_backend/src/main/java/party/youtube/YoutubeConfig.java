2fdc28271c810e96aef0881e45d3195a54534f97
Marek Pokropiński <marek.pokropinski@outlook.com>
2018-09-30 13:59:45 +0200

    changed access to google api from oauth to api key

--- YouParty_backend/src/main/java/party/youtube/YoutubeConfig.java
+++ YouParty_backend/src/main/java/party/youtube/YoutubeConfig.java
@@ -1,28 +1,19 @@
 package party.youtube;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.List;
+import java.security.GeneralSecurityException;
 
 import org.jboss.logging.Logger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.youtube.YouTube;
-import com.google.api.services.youtube.YouTubeScopes;
 
 @Configuration
 @Profile(value = { "dev", "prod" })
@@ -32,61 +23,21 @@ public class YoutubeConfig {
 
 	private static final Logger LOG = Logger.getLogger(YoutubeConfig.class);
 
-	/** Directory to store user credentials for this application. */
-	private static final java.io.File DATA_STORE_DIR = new java.io.File(System.getProperty("user.home"),
-			".credentials/youparty");
-
-	/** Global instance of the {@link FileDataStoreFactory}. */
-	private static FileDataStoreFactory DATA_STORE_FACTORY;
-
-	/** Global instance of the JSON factory. */
-	private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
-
-	/** Global instance of the HTTP transport. */
-	private static HttpTransport HTTP_TRANSPORT;
-
-	/**
-	 * Global instance of the scopes required by this quickstart.
-	 *
-	 * If modifying these scopes, delete your previously saved credentials at
-	 * ~/.credentials/drive-java-quickstart
-	 */
-	private static final List<String> SCOPES = Arrays.asList(YouTubeScopes.YOUTUBE_READONLY);
-
-	static {
-		try {
-			HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-			DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR);
-		} catch (Throwable t) {
-			t.printStackTrace();
-			System.exit(1);
-		}
-	}
-
-	/**
-	 * Create an authorized Credential object.
-	 */
-	public static Credential authorize() throws IOException {
-		// Load client secrets.
-		InputStream in = YoutubeConfig.class.getResourceAsStream("/client_secret.json");
-		GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
-
-		// Build flow and trigger user authorization request.
-		GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY,
-				clientSecrets, SCOPES).setDataStoreFactory(DATA_STORE_FACTORY).setAccessType("offline").build();
-		Credential credential = new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
-		return credential;
-	}
-
-	/**
-	 * Build and return an authorized API client service, such as a YouTube Data API
-	 * client service.
-	 */
 	@Bean
 	public static YouTube getYouTubeService() throws IOException {
 		LOG.info("Connecting with YouTube API serivce");
-		Credential credential = authorize();
-		return new YouTube.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential).setApplicationName(APPLICATION_NAME)
-				.build();
+
+		HttpTransport HTTP_TRANSPORT;
+		try {
+			HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+			JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+			return new YouTube.Builder(HTTP_TRANSPORT, JSON_FACTORY, (HttpRequest request) ->
+				{
+				}).setApplicationName(APPLICATION_NAME).build();
+		} catch (GeneralSecurityException e) {
+			LOG.error("failed to initialize google http transport");
+			System.exit(1);
+			return null;
+		}
 	}
 }
