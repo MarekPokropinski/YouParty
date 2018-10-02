892485f5ddac2d3dd0da99993c553a789aabfaab
Marek Pokropiński <marek.pokropinski@outlook.com>
2018-10-02 17:25:30 +0200

    Changed environment production to heroku, fixed websocket

--- YouParty_frontend/src/environments/environment.prod.ts
+++ YouParty_frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  backendUrl: "http://156.17.226.48:8888/youparty",
-  wsUrl: "ws://156.17.226.48:8888/youparty"
+  backendUrl: "https://you-party.herokuapp.com",
+  wsUrl: "wss://you-party.herokuapp.com"
 };
